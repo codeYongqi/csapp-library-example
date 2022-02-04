b1e6527ff64edb0de232f8d6c17d0c343f38dbc7
Kirin Zhu <zhuyongqi9@gmail.com>
2022-02-04 10:54:38 +0800

    Update hostinfo.c

--- netp/hostinfo.c
+++ netp/hostinfo.c
@@ -28,29 +28,3 @@ int main(int argc, char **argv) {
 
 	exit(0);
 }
-
-int open_listenfd(char *hostname, char *port) {
-	int listenfd, optval=1;
-	struct addrinfo hints, *listp, *p;
-
-	memset(&hints, 0, sizeof(struct addrinfo));
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_NUMERICSERV;
-	hints.ai_flags |= AI_ADDRCONFIG;
-	Getaddrinfo(hostname, port, &hints, &listp);
-
-	for (p = listp; p; p = p->ai_next) {
-		if ((clientfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) < 0)
-			continue;
-		
-		if (connect(clientfd, p->ai_addr, p->ai_addrlen) != -1) 
-			break;
-		Close(clientfd);
-	}
-
-	Freeaddrinfo(listp);
-	if (!p)
-		return -1;
-	else
-		return clientfd;
-}
